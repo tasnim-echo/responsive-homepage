d809ef8971f84e84efbd8ae3ff270373ad18b6d9
tasnim-echo <tasnim@echologyx.com>
2026-02-23 12:01:20 +0600

    mobile view is done

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const nav = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 
 window.addEventListener("scroll", function () {
   if (pageYOffset > 60) {
     nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
   }
+});
+
+menuIcon.addEventListener("click", function () {
+  mobileMenuContainer.classList.add("active");
+});
+closeIcon.addEventListener("click", function () {
+  mobileMenuContainer.classList.remove("active");
 });
